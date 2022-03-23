2bfc9797e55ee2b706d364c2b5847526b8cbada0
NGyc <1198974961@qq.com>
2022-03-23 23:20:40 +0800

    '测试上传'

---
+++ build/commit.js
@@ -0,0 +1,12 @@
+const yargs = require('yargs');
+
+const childProcess = require('child_process');
+
+const { _: message } = yargs.argv;
+
+if (message) {
+  childProcess.execSync('git  pull');
+  childProcess.execSync('git add .');
+  childProcess.execSync(`git commit -m '${message}'`);
+  childProcess.execSync('git push -u orgin master');
+}
